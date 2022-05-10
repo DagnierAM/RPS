d99b6c960712d0db05c604a73f329640467f50ed
Dagnier <dagniermorales@icloud.com>
2022-05-10 11:16:43 -0400

    Change rock Image

--- README.md
+++ README.md
@@ -4,6 +4,14 @@ Odin Project's rock paper sisscor project. I will be using functions to create a
 This isn't the prettiest thing in the world but my css knowledge is limited at this point in time. I hope that pretty soon Ill be able to make projects like this with more beautiful UI's and nice animiations.
 
 Image credits:
-1.https://cdn5.vectorstock.com/i/1000x1000/64/24/gray-rock-stone-on-a-black-background-vector-18756424.jpg
-2.https://upload.wikimedia.org/wikipedia/commons/2/2f/Paper_450x450.jpg
-3.https://media.istockphoto.com/photos/professional-scissors-on-black-background-picture-id1124646454?k=20&m=1124646454&s=170667a&w=0&h=HQmigpJ668ENNcvDjkh96Siuvyv8YkSYpDUqo_kPTOc=
+<oll>
+    <li>
+        https://images.unsplash.com/photo-1525857597365-5f6dbff2e36e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80
+    </li>
+    <li>
+        https://upload.wikimedia.org/wikipedia/commons/2/2f/Paper_450x450.jpg
+    </li>
+    <li>
+        https://media.istockphoto.com/photos/professional-scissors-on-black-background-picture-id1124646454?k=20&m=1124646454&s=170667a&w=0&h=HQmigpJ668ENNcvDjkh96Siuvyv8YkSYpDUqo_kPTOc=
+    </li>
+</ol>
--- scripts.js
+++ scripts.js
@@ -1,42 +1,42 @@
-let playerWins = 0, compWins = 0; 
+let playerWins = 0, compWins = 0;
 const results = document.querySelector('#results');
 const playerScore = document.querySelector('#playerScore');
 const computerScore = document.querySelector('#computerScore');
 const images = document.querySelectorAll('img');
 const button = document.querySelector('button');
 
-function computerPlay(){
-    const compOptions = ['ROCK', 'PAPER','SISSCORS'];
+function computerPlay() {
+    const compOptions = ['ROCK', 'PAPER', 'SISSCORS'];
     const choiceNum = Math.floor(Math.random() * 3);
     return compOptions[choiceNum];
 }
 
-function playRound(playerSelection, computerSelection){
+function playRound(playerSelection, computerSelection) {
     //const caseInPlayerSel = playerSelection.toUpperCase();
     //const caseInCompSel = computerSelection.toUpperCase();
-    if(playerSelection ===  computerSelection){
+    if (playerSelection === computerSelection) {
         results.textContent = 'Looks like you tied!';
-    }else if(playerSelection === 'ROCK'){
-        if(computerSelection === 'SISSCORS'){
+    } else if (playerSelection === 'ROCK') {
+        if (computerSelection === 'SISSCORS') {
             playerScore.textContent = ++playerWins;
             results.textContent = 'You Win! Rock beats Sisscors';
-        }else{
+        } else {
             computerScore.textContent = ++compWins;
             results.textContent = 'You lose! Paper beats Rock';
         }
-    }else if(playerSelection === 'PAPER'){
-        if(computerSelection === 'ROCK'){
+    } else if (playerSelection === 'PAPER') {
+        if (computerSelection === 'ROCK') {
             playerScore.textContent = ++playerWins;
             results.textContent = 'You Win! Paper beats Rock';
-        }else{
+        } else {
             computerScore.textContent = ++compWins;
             results.textContent = 'You lose! Sisscors beats Paper';
         }
-    }else if(playerSelection === 'SISSCORS'){
-        if(computerSelection === 'PAPER'){
+    } else if (playerSelection === 'SISSCORS') {
+        if (computerSelection === 'PAPER') {
             playerScore.textContent = ++playerWins;
             results.textContent = 'You win! Sisscors beats Paper';
-        }else{
+        } else {
             computerScore.textContent = ++compWins;
             results.textContent = 'You lose! Rock beats Sisscors';
         }
@@ -44,16 +44,16 @@ function playRound(playerSelection, computerSelection){
     checkWinner();
 }
 
-function userInput(e){
+function userInput(e) {
     let playerChoice = this.classList.value;
-    e.stopPropagation(); 
+    e.stopPropagation();
     this.classList.add('playing');
     playRound(playerChoice, computerPlay());
 }
 images.forEach(img => img.addEventListener('transitionend', removeTransition));
 
-function removeTransition(e){
-    if(e.propertyName !== 'transform') return; // skip it if its not a transform
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return; // skip it if its not a transform
     this.classList.remove('playing');
 }
 
@@ -64,11 +64,11 @@ button.addEventListener('click', () => {
     return false;
 });
 
-function checkWinner(){
-    if(playerWins === 5){
+function checkWinner() {
+    if (playerWins === 5) {
         results.textContent = 'Congratulations You\'ve Won!';
         images.forEach(img => img.removeEventListener('click', userInput));
-    }else if(compWins === 5){
+    } else if (compWins === 5) {
         results.textContent = 'Bummer! You\'ve Lost to a Machine!';
         images.forEach(img => img.removeEventListener('click', userInput));
     }
